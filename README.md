676e73843d6f0da6a6e767b4ae26a2990fe1ca8d
wad <suguby@gmail.com>
2016-08-11 11:41:30 +0300

    packaging

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # simple_draw
+
+Pygame-based package for drawing graphical primitives for tutorial purpose.
+
+## Install pygame on Windows.
+ 
+* Download needed .whl from http://www.lfd.uci.edu/~gohlke/pythonlibs/#pygame
+* With python > 3.5
+    cd Downloads
+    pip install pygame-xxxxxx.whl
+* With python 2.7
+    ...coming soon...
+
+## Install pygame on Debian-linux. 
+
+* With python 2.7
+    sudo aptitude install python-pygame
+* With python 3.x
+    sudo apt-get install python3-pip python3-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python3-numpy libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
+    sudo pip3 install -U hg+https://bitbucket.org/pygame/pygame
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup
+
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='simple_draw',
+    version='2.4.0',
+    py_modules=["simple_draw"],
+    license='BSD License',
+    description='This package allows you to draw graphical primitives with pygame.',
+    long_description=README,
+    url='https://github.com/suguby/simple_draw',
+    author='Shandrinov Vadim',
+    author_email='suguby@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Education',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Education',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='draw graphical primitives tutorial',
+    # install_requires=[
+    #     'pygame'
+    # ]
+)
