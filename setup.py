fb924712a1db88346f70f6fc8a8e80f028f1f816
Waddik <vadim.shandrinoff@moex.com>
2020-07-09 13:45:55 +0300

    lesson 004-04: зачет

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='simple_draw',
-    version='2.6.8',
+    version='2.6.9.dev1',
     py_modules=["simple_draw"],
     license='BSD License',
     description='This package allows you to draw graphical primitives with pygame.',
@@ -32,6 +32,6 @@ setup(
     ],
     keywords='draw graphical primitives tutorial',
     install_requires=[
-        'pygame==2.0.0.dev8'
+        'pygame==2.0.0.dev10'
     ]
 )
